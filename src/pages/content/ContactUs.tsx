05a996e90f250fca92de7ae7a0fa15453e84bc56
marisaraj <marisa.raj@blu-ocean.net>
2022-11-13 20:16:13 +0530

    Update

--- src/pages/content/ContactUs.tsx
+++ src/pages/content/ContactUs.tsx
@@ -14,12 +14,13 @@ export default function ContactUs() {
           If you believe {" "}
           <span className="contactUsTitle_bold">we’re right for you.</span>
         </span>
-        <p className="contactUsEmail">
+        <div>
+        <span className="contactUsEmail">
           Write to us at{" "}
-          <a href="mailto:admin@techparsona.com">admin@techparsona.com</a>
-        </p>
-        <motion.div  whileHover={{ scaleY: 1.05}}     whileTap={{ scale: 0.9 }}>
-          <a href="mailto:admin@techparsona.com" className="contactusButton">
+         <span><a href="mailto:admin@techparsona.com">admin@techparsona.com</a></span> 
+        </span></div>
+        <motion.div  whileHover={{ scaleY: 1.05}}  whileTap={{ scale: 0.9 }} className="contactusButton">
+          <a href="mailto:admin@techparsona.com" className="contactusButton_main">
             Contact Us
           </a>
         </motion.div>
