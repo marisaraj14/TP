aef66a6332b8093ab0c3978abd044bcb0c988fd6
marisaraj <marisa.raj@blu-ocean.net>
2022-11-21 20:09:21 +0530

    "SEO"

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import MainBlock from "./pages/content/MainBlock";
-import './styles/index.scss'
+import "./styles/index.scss";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 function App() {
   return (
-      <MainBlock/>
+    <HelmetProvider>
+      <Helmet>
+        <title>
+          Tech Parsona - We provide the right Oracle solutions for you.
+        </title>
+        <meta
+          name="description"
+          content=" TechParsona is created by seasoned experts with a track record of
+          professional, academic, and technological achievements."
+        />
+        ;
+      </Helmet>
+      <MainBlock />
+    </HelmetProvider>
   );
 }
 
