7c3863e7fd9c38e59ada207d23d2451c7dda5cd3
marisaraj <marisa.raj@blu-ocean.net>
2022-12-20 00:03:44 +0530

    "Logo"

--- src/pages/components/NavBar.tsx
+++ src/pages/components/NavBar.tsx
@@ -9,65 +9,88 @@ export default function NavBar() {
   const [modal, setOpenModal] = useState<boolean>(false);
 
   return (
-    <>{modal && (
-      <div className="modalContainer">
-        <img src="/media/closeicon.png" className="modalContainer_close" alt="close icon"
-        onClick={()=> setOpenModal(false)}/>
-        <div className="modalContainer_sub">
-          {fields.map((item) => (
+    <>
+      {modal && (
+        <div className="modalContainer">
+          <img
+            src="/media/closeicon.png"
+            className="modalContainer_close"
+            alt="close icon"
+            onClick={() => setOpenModal(false)}
+          />
+          <div className="modalContainer_sub">
+            {fields.map((item) => (
+              <a
+                href={item.path}
+                onClick={() => setOpenModal(false)}
+                className="modalContainer_title"
+              >
+                {item.name}
+              </a>
+            ))}
             <a
-              href={item.path}
+              href="mailto:admin@techparsona.com"
+              className="modalContainer_button"
               onClick={() => setOpenModal(false)}
-              className="modalContainer_title"
             >
-              {item.name}
+              Contact Us
             </a>
-          ))}
-          <a href="mailto:admin@techparsona.com" className="modalContainer_button"
-          onClick={() => setOpenModal(false)}>
-            Contact Us
-          </a>
+          </div>
         </div>
-      </div>
-    )}
-    <div className="navContainer">
-      <Layout>
-        <motion.p
-          animate={{ y: 10 }}
-          transition={{ type: "spring", duration: 0.7 }}
-        >
-          <div className="navContainer__flex">
-            <span className="logoText">
+      )}
+      <div className="navContainer">
+        <Layout>
+          <motion.p
+            animate={{ y: 10 }}
+            transition={{ type: "spring", duration: 0.7 }}
+          >
+            <div className="navContainer__flex">
+              <div className="navContainer__flexbox ">
+                <img
+                  src="./media/textlogo.png"
+                  alt="Logo"
+                  className="navContainer__image"
+                />
+
+                <img
+                  src="./media/logotext_2.png"
+                  alt="Logo"
+                  className="navContainer__imagetext"
+                />
+              </div>
+              {/* <span className="logoText">
               Tech <span className="logoText__color">Parsona</span>
-            </span>
-            <img
-              src="/media/svg/navBarIcon.svg"
-              alt="links"
-              className="linkContainer"
-              onClick={() => setOpenModal(!modal)}
-            />
-            <div className="navBarContainer">
-              {fields.map((item) => (
+            </span> */}
+              <img
+                src="/media/svg/navBarIcon.svg"
+                alt="links"
+                className="linkContainer"
+                onClick={() => setOpenModal(!modal)}
+              />
+              <div className="navBarContainer">
+                {fields.map((item) => (
+                  <motion.button
+                    whileHover={{ scale: 1.1 }}
+                    animate={{ color: "#4B8EB0" }}
+                  >
+                    <AnchorLink className="fields" href={item.path}>
+                      {item.name}
+                    </AnchorLink>
+                  </motion.button>
+                ))}
                 <motion.button
                   whileHover={{ scale: 1.1 }}
-                  animate={{ color: "#4B8EB0" }}
+                  className="navButtonContainer"
                 >
-                  <AnchorLink className="fields" href={item.path}>
-                    {item.name}
-                  </AnchorLink>
+                  <a href="mailto:admin@techparsona.com" className="navButton">
+                    Contact
+                  </a>
                 </motion.button>
-              ))}
-              <motion.button whileHover={{ scale: 1.1 }} className="navButtonContainer">
-                <a href="mailto:admin@techparsona.com" className="navButton">
-                  Contact
-                </a>
-              </motion.button>
+              </div>
             </div>
-          </div>
-        </motion.p>
-      </Layout>
-     
-    </div>
+          </motion.p>
+        </Layout>
+      </div>
     </>
   );
 }
