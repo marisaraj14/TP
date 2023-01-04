20928472fa87d164b9690d64e56c87844bee4057
marisaraj <marisa.raj@blu-ocean.net>
2023-01-04 23:41:02 +0530

    Added og

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import Head from "next/head"
+import Head from "next/head";
 import MainBlock from "./sections/content/MainBlock";
 // import "../index.css"
 
@@ -6,26 +6,30 @@ export default function Home() {
   return (
     <>
       <Head>
-        <link rel="icon" href="media/textlogo.png" />
+        <link rel="icon" href="./media/logo.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
         <title>
           Tech Parsona - We provide the right Oracle solutions for you.
         </title>
         <link type="stylesheet" href="../src/styles/index.scss" />
-        <link
-          rel="canonical"
-          href="https://techparsona.com/"
-          key="canonical"
-        />
+        <link rel="canonical" href="https://techparsona.com/" key="canonical" />
         <meta
           name="description"
           content="TechParsona is created by seasoned experts with a track record of
     professional, academic, and technological achievements."
         />
+        <meta property="og:title" content="Tech Parsona - We provide the right Oracle solutions for you." />
+        <meta
+          property="og:description"
+          content="TechParsona is created by seasoned experts with a track record of
+          professional, academic, and technological achievements."
+        />
+        <meta property="og:url" content="https://techparsona.com/" />
+        <meta property="og:type" content="website" />
       </Head>
       <main>
-       <MainBlock/>
+        <MainBlock />
       </main>
     </>
   );
